5c50068703ea3999b5ca32a4fc4271a89e6b60e4
Martin Sadovy <sodae@email.cz>
2020-06-25 13:53:58 +0200

    fix composer path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@
 envsubst < /opt/satis-go/config.template.yaml > /opt/satis-go/config.yaml
 
 if [[ $GITHUB_TOKEN ]]; then
-    composer config -g github-oauth.github.com $GITHUB_TOKEN
+    /satis/vendor/bin/composer config -g github-oauth.github.com $GITHUB_TOKEN
 fi
 
 if [[ -n $GITLAB_TOKEN ]] && [[ -n $GITLAB_HOST ]]; then
-    composer config -g "gitlab-token.${GITLAB_HOST}" "${GITLAB_TOKEN}"
-    composer config -g gitlab-domains "$GITLAB_HOST" "gitlab.com"
+    /satis/vendor/bin/composer config -g "gitlab-token.${GITLAB_HOST}" "${GITLAB_TOKEN}"
+    /satis/vendor/bin/composer config -g gitlab-domains "$GITLAB_HOST" "gitlab.com"
 fi
 
 exec "$@"
